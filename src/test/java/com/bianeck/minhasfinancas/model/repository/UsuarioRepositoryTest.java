f098f4133d7e63876548487c871472510e815de7
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-09-08 23:02:19 -0300

    34. Adicionando Banco de dados em Memória para os testes de Integração

--- src/test/java/com/bianeck/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/bianeck/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
     @Autowired
