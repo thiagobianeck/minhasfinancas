ea33a93b96113548b87a8f086eb529e8f351dabf
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-09-10 10:50:11 -0300

    39. Melhorando nossos testes de integração com @DataJpaTest

--- src/test/java/com/bianeck/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/bianeck/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -6,13 +6,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioRepositoryTest {
 
     @Autowired
